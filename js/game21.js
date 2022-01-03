fa5f995d800a2e07885485ff376ba62ba127df1d
AlexSashik <espiritudelnumero@gmail.com>
2022-01-03 15:29:33 +0200

    game 2021

---
+++ js/game21.js
@@ -0,0 +1,419 @@
+let allImages = {
+    jan: [
+        ['img/game21/jan/16/01-min.jpg', '2 и 16 января: прогулка по Сумской и саду Шевченко', 'jan', [
+            'img/game21/jan/02/02-min.jpg',
+            'img/game21/jan/02/03-min.jpg',
+            'img/game21/jan/02/04-min.jpg',
+            'img/game21/jan/02/05-min.jpg',
+            'img/game21/jan/02/06-min.jpg',
+            'img/game21/jan/16/03-min.jpg'
+        ]],
+        ['img/game21/jan/21/02-min.jpg', '21 января: квадрики', 'jan', [
+            'img/game21/jan/21/01-min.jpg',
+            'img/game21/jan/21/03-min.jpg',
+            'img/game21/jan/21/04-min.jpg'
+        ]],
+    ],
+    feb: [
+        ['img/game21/feb/06/3-min.jpg', '6 февраля: покатушки на коньках', 'feb', [
+            'img/game21/feb/06/1-min.jpg',
+            'img/game21/feb/06/2-min.jpg',
+            'img/game21/feb/06/4-min.jpg'
+        ]],
+        ['img/game21/feb/14/1-min.jpg', '14 февраля: средняя прожарка', 'feb', [
+            'img/game21/feb/14/3-min.jpg',
+            'img/game21/feb/14/2-min.jpg',
+            'img/game21/feb/14/4-min.jpg'
+        ]],
+    ],
+    mar: [
+        ['img/game21/mar/08/4-min.jpg', '8 марта и праздник желудка', 'mar', [
+            'img/game21/mar/08/1-min.jpg',
+            'img/game21/mar/08/2-min.jpg',
+            'img/game21/mar/08/3-min.jpg',
+        ]]
+    ],
+    apr: [
+        ['img/game21/apr/17/5-min.jpg', '17 апреля: поселковая экстрим-зона', 'apr', [
+            'img/game21/apr/17/6-min.jpg',
+            'img/game21/apr/17/2-min.jpg',
+            'img/game21/apr/17/3-min.jpg',
+        ]]
+    ],
+    may: [
+        ['img/game21/may/28-min.jpg', '28 мая: не прошло и пол года...', 'may', []],
+        ['img/game21/may/22/3-min.jpg', '22 мая: агрессивный Cема', 'may', [
+            'img/game21/may/22/1-min.jpg',
+            'img/game21/may/22/2-min.jpg',
+        ]]
+    ],
+    jun: [
+        ['img/game21/jun/9/4-min.jpg', '9 июня: Опошня', 'jun', [
+            'img/game21/jun/9/1-min.jpg',
+            'img/game21/jun/9/2-min.jpg',
+            'img/game21/jun/9/3-min.jpg'
+        ]],
+        ['img/game21/jun/19/4-min.jpg', '19 июня: электрификация кисюнского особняка', 'jun', [
+            'img/game21/jun/19/1-min.jpg',
+            'img/game21/jun/19/2-min.jpg',
+            'img/game21/jun/19/3-min.jpg'
+        ]]
+    ],
+    jul: [
+        ['img/game21/jul/7/1-min.jpg', '7 июля: чистиньким нужно быть всегда!', 'jul', []],
+        ['img/game21/jul/10-min.jpg', '10 июля: изучение волосни в парке Горького', 'jul', []],
+        ['img/game21/jul/24-min.jpg', '24 июля: проект "Весенний венок"', 'jul', []],
+        ['img/game21/jul/23/1-min.jpg', '23 июля: S-Park', 'jul', [
+            'img/game21/jul/23/2-min.jpg',
+            'img/game21/jul/23/3-min.jpg',
+            'img/game21/jul/23/4-min.jpg'
+        ]],
+        ['img/game21/jul/29/1-min.jpg', '29 июля: рефакторинг ванны', 'jul', [
+            'img/game21/jul/29/2-min.jpg',
+            'img/game21/jul/29/3-min.jpg',
+            'img/game21/jul/29/4-min.jpg'
+        ]]
+    ],
+    aug: [
+        ['img/game21/aug/9-13/1-min.jpg', '9-13 августа: деревенский чилл', 'aug', [
+            'img/game21/aug/9-13/2-min.jpg',
+            'img/game21/aug/9-13/3-min.jpg',
+            'img/game21/aug/9-13/4-min.jpg'
+        ]],
+        ['img/game21/aug/22/1-min.jpg', '22 августа: Пушка, Горький, ворота зоопарка', 'aug', [
+            'img/game21/aug/22/2-min.jpg',
+            'img/game21/aug/22/3-min.jpg',
+            'img/game21/aug/22/4-min.jpg'
+        ]],
+        ['img/game21/aug/28/2-min.jpg', '28 августа: высоко сижу, далеко гляжу', 'aug', [
+            'img/game21/aug/28/1-min.jpg',
+            'img/game21/aug/28/3-min.jpg',
+            'img/game21/aug/28/4-min.jpg'
+        ]]
+    ],
+    sep: [
+        ['img/game21/sep/12/1-min.jpg', '12 сентября: о чем говорят лошади и белки', 'sep', [
+            'img/game21/sep/12/2-min.jpg',
+            'img/game21/sep/12/3-min.jpg',
+            'img/game21/sep/12/4-min.jpg',
+            'img/game21/sep/12/5-min.jpg',
+            'img/game21/sep/12/6-min.jpg',
+        ]],
+        ['img/game21/sep/14/2-min.jpg', '5 и 14 сентября: Тедди обыкновенный и венецианский ', 'sep', [
+            'img/game21/sep/14/1-min.jpg',
+            'img/game21/sep/14/3-min.jpg',
+            'img/game21/sep/14/5-min.jpg',
+        ]],
+        ['img/game21/sep/26-min.jpg', '26 сентября: корона приходит и уходит, а Slayer остается навсегда', 'sep', []],
+    ],
+    oct: [
+        ['img/game21/oct/3/1-min.jpg', '3 октября: выставка кошек ', 'oct', [
+            'img/game21/oct/3/2-min.jpg',
+            'img/game21/oct/3/3-min.jpg',
+            'img/game21/oct/3/4-min.jpg',
+            'img/game21/oct/3/5.gif',
+        ]],
+        ['img/game21/oct/10/3-min.jpg', '10 октября: зоопарк и хризантемы ', 'oct', [
+            'img/game21/oct/10/1-min.jpg',
+            'img/game21/oct/10/2-min.jpg',
+            'img/game21/oct/10/4-min.jpg'
+        ]],
+        ['img/game21/oct/23/1-min.jpg', '23 октября: кисюнство вооружено и очень опасно', 'oct', [
+            'img/game21/oct/23/4-min.jpg',
+            'img/game21/oct/23/2-min.jpg',
+            'img/game21/oct/23/3-min.jpg',
+            'img/game21/oct/23/5.gif'
+        ]],
+        ['img/game21/oct/24/1-min.jpg', '24 октября: даешь молодежь!', 'oct', [
+            'img/game21/oct/24/2-min.jpg',
+            'img/game21/oct/24/3-min.jpg',
+            'img/game21/oct/24/4-min.jpg',
+        ]],
+        ['img/game21/oct/30-min.jpg', '30 октября: ужасно сладкий праздник', 'oct', []],
+    ],
+    nov: [
+        ['img/game21/nov/14/4-min.jpg', '14 ноября: космическое кисюнство', 'nov', [
+            'img/game21/nov/14/1-min.jpg',
+            'img/game21/nov/14/3-min.jpg',
+            'img/game21/nov/14/2-min.jpg',
+        ]],
+        ['img/game21/nov/19-min.jpg', '19 ноября: мужская Котодиана', 'nov', []],
+        ['img/game21/nov/21/3-min.jpg', '21 ноября: фэнтези картинг', 'nov', [
+            'img/game21/nov/21/4-min.jpg',
+            'img/game21/nov/21/5-min.jpg',
+            'img/game21/nov/21/6-min.jpg',
+            'img/game21/nov/21/2-min.jpg',
+            'img/game21/nov/21/1-min.jpg',
+            'img/game21/nov/21/7-min.jpg',
+        ]],
+    ],
+    dec: [
+        ['img/game21/dec/4/1-min.jpg', '4 декабря: персональный карт', 'dec', [
+            'img/game21/dec/4/2-min.jpg',
+            'img/game21/dec/4/3-min.jpg',
+            'img/game21/dec/4/4-min.jpg',
+            'img/game21/dec/4/5-min.jpg',
+        ]],
+        ['img/game21/dec/13-min.jpg', '13 декабря: перезагрузка оптики', 'dec', []],
+        ['img/game21/dec/20-min.jpg', '20 декабря', 'dec', []],
+        ['img/game21/dec/26.gif', '26 декабря', 'dec', []],
+        ['img/game21/dec/27/1-min.jpg', '27 декабря: медвежий фотосет', 'dec', [
+            'img/game21/dec/27/2-min.jpg',
+            'img/game21/dec/27/3-min.jpg',
+        ]],
+    ]
+};
+
+let isItVeryFirstClick = true;
+let seasonimages = [];
+let currentImageNumber = 0;
+let mainImg = $('#main_img');
+let extraImg = $('.extra-photos');
+let descriptionBlock = $('#description');
+let monthClickLocker = false;
+let nextImgBtn = $('#next_photo');
+let xp = 0;
+let activeSeason;
+let completedSeasons = {
+    'loveseason': false,
+    'chillseason': false,
+    'workseason': false
+};
+let rank = 'Кися';
+
+Swal.fire(
+    'Добро пожаловать!',
+    'Давай вспомним яркие моменты ушедшего 2021 года. Выбирай сезон и начинай игру!',
+    'success'
+);
+
+/**
+ * Start
+ */
+$('#loveseason').click(function (el) {
+
+    if (completedSeasons.loveseason)
+        return;
+    $('#quiz h2').text('Сезон любви');
+    activateQuiz(el, 'jan', 'feb', 'mar', 'apr');
+    activeSeason = 'loveseason';
+});
+
+$('#chillseason').click(function (el) {
+    if (completedSeasons.chillseason)
+        return;
+    $('#quiz h2').text('Сезон отдыха');
+    activateQuiz(el, 'may', 'jun', 'jul', 'aug');
+    activeSeason = 'chillseason';
+});
+
+$('#workseason').click(function (el) {
+    if (completedSeasons.workseason)
+        return;
+    $('#quiz h2').text('Сезон работы');
+    activateQuiz(el, 'sep', 'oct', 'nov', 'dec');
+    activeSeason = 'workseason';
+});
+
+/**
+ * Attempt
+ */
+$('.month').click(function (el) {
+    if (monthClickLocker)
+        return;
+    monthClickLocker = true;
+
+    let currentDescription = seasonimages[currentImageNumber][1];
+    let currentMonth = seasonimages[currentImageNumber][2];
+    let extraPhotos = seasonimages[currentImageNumber][3];
+
+    $('.month').addClass('noHover');
+    if (el.target.className.split(' ').includes(currentMonth)) {
+        addXP();
+        $(this).addClass('success');
+        $('#tiama').css('display', 'block').children("img").css('borderColor', '#30ee50').attr("src","img/game21/contento-min.jpg");
+    } else {
+        $(this).addClass('wrong');
+        $('#tiama').css('display', 'block').children("img").css('borderColor', 'red').attr("src","img/game21/sramo-min.jpg");
+        $('.'+currentMonth).addClass('success');
+    }
+    descriptionBlock.text(currentDescription);
+    nextImgBtn.css('display', 'block');
+
+    if (!seasonimages[currentImageNumber + 1])
+        nextImgBtn.text('На главную ❯');
+
+    for (let i = 0; i < extraPhotos.length; i++) {
+        extraImg.append('<img src="' + extraPhotos[i] + '" alt="">');
+    }
+
+});
+
+/**
+ * Click next month
+ */
+nextImgBtn.click(function () {
+    $(this).css('display', 'none');
+    currentImageNumber++;
+    if (seasonimages[currentImageNumber]) {
+        removeSuccessWrongClasses();
+        $('#tiama').css('display', 'none');
+        extraImg.children("img").remove();
+        shiftMainImg();
+        monthClickLocker = false;
+    } else {
+        if (activeSeason === 'loveseason') {
+            $('#loveseason').removeClass('flex-block-active');
+            $('#loveCheck').css('display', 'inline-block');
+            completedSeasons.loveseason = true;
+        } else if (activeSeason === 'chillseason') {
+            $('#chillseason').removeClass('flex-block-active');
+            $('#chillCheck').css('display', 'inline-block');
+            completedSeasons.chillseason = true;
+        } else if (activeSeason === 'workseason') {
+            $('#workseason').removeClass('flex-block-active');
+            $('#workCheck').css('display', 'inline-block');
+            completedSeasons.workseason = true;
+        }
+
+        if (completedSeasons.loveseason && completedSeasons.chillseason && completedSeasons.workseason) {
+            $('#filnal-text').text('С Новым Годом, ' + rank + '!!!')
+            $('.final-block').css('display', 'block');
+        }
+        $('#main').removeClass('hidden');
+        $('#quiz').addClass('hidden');
+        resetAll();
+    }
+});
+
+/**
+ *
+ * @param el
+ * @param firstMonth
+ * @param secondMonth
+ * @param ThirdMonth
+ * @param fourthMonth
+ */
+function activateQuiz(el, firstMonth, secondMonth, ThirdMonth, fourthMonth) {
+    let headerForQuiz = el.delegateTarget.childNodes[4].innerText;
+
+    if (isItVeryFirstClick) {
+        Swal.fire(
+            'Правила игры',
+            'Угадывай месяцы счатсливых моментов 2021 года, зарабатывай тямапоинты, будь Кисей!',
+            'success'
+        );
+        isItVeryFirstClick = false;
+    }
+
+    $('.rank').css('display', 'block');
+    $('#quiz h2').text(headerForQuiz);
+    $('#quiz').removeClass('hidden');
+    $('#main').addClass('hidden');
+    $('#first-month').addClass(firstMonth);
+    $('#second-month').addClass(secondMonth);
+    $('#third-month').addClass(ThirdMonth);
+    $('#fourth-month').addClass(fourthMonth);
+    seasonimages.length = 0;
+    seasonimages = seasonimages.concat(allImages[firstMonth], allImages[secondMonth], allImages[ThirdMonth], allImages[fourthMonth]);
+    seasonimages = shuffle(seasonimages);
+    shiftMainImg();
+
+}
+
+/**
+ *
+ * @param removeMonthClasses
+ */
+function removeSuccessWrongClasses (removeMonthClasses = false) {
+    let allMonthBlocks = $('.month');
+
+    allMonthBlocks.removeClass('success wrong');
+    allMonthBlocks.removeClass('noHover');
+
+    if (removeMonthClasses)
+        allMonthBlocks.removeClass('jan may sep feb jun, oct mar jul nov apr aug dec');
+}
+
+/**
+ *
+ */
+function shiftMainImg () {
+    descriptionBlock.text('');
+    mainImg.children("img").remove();
+    mainImg.append('<img src="'+seasonimages[currentImageNumber][0]+'" alt="">');
+}
+
+/**
+ *
+ */
+function resetAll () {
+    seasonimages.length = 0;
+    currentImageNumber = 0;
+    monthClickLocker = false;
+    removeSuccessWrongClasses(true);
+    mainImg.children("img").remove();
+    extraImg.children("img").remove();
+    nextImgBtn.css('display', 'none').text('Следующий эпизод ❯');
+    $('#tiama').css('display', 'none');
+}
+
+/**
+ *
+ */
+function addXP () {
+    xp++;
+    $('#xp').text(xp);
+    $('#mainxp').text(xp);
+    levelUp(xp, 10, 'МегаКися', -65, 0);
+    levelUp(xp, 20, 'ТераМегаКися',-140, 0);
+    levelUp(xp, 25, 'ЗетаТераМегаКися',0, -60);
+    levelUp(xp, 30, 'ЭкстраЗетаТераМегаКися',-65, -60);
+}
+
+/**
+ *
+ * @param currentXp
+ * @param levelUpXp
+ * @param newRank
+ * @param bgposx
+ * @param bgposy
+ */
+function levelUp(currentXp, levelUpXp, newRank, bgposx, bgposy) {
+    if (currentXp === levelUpXp) {
+        rank = newRank;
+        Swal.fire(
+            'Новый уровень!',
+            'Поздравляю, ты набрала ' + currentXp + ' тямопоинтов и теперь ты ' + rank + '!',
+            'success'
+        );
+        $('#rank').text(rank);
+        $('#levelImg').css('background-position-x', bgposx+'px').css('background-position-y', bgposy+'px');
+    }
+}
+
+/**
+ * Shuffle the array
+ *
+ * @param {array} array
+ * @returns {array}
+ */
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
